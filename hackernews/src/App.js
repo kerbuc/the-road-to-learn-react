c1d98a6ce1e5eb5dd4250a0d7402d372a677c17e
Arber Osmani <arber.osmani.ao@gmail.com>
2017-11-10 19:13:01 +0100

    Uebung

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -20,13 +20,19 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
   }
 
   setSearchTopStories(result) {
@@ -44,7 +50,7 @@ class App extends Component {
 
     this.setState({
       results: {
-        ...result,
+        ...results,
         [searchKey]: { hits: updatedHits, page}
       }
     });
@@ -54,9 +60,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(e => {
-        console.error("Unerwarteter Fehler")
-      });
+      .catch(error => this.setState({ error }));
   }
 
   onSearchChange(event) {
@@ -66,16 +70,26 @@ class App extends Component {
   onSearchSubmit(event) {
     const searchTerm = this.state.searchTerm;
     this.setState({ searchKey: searchTerm });
-    this.fetchSearchTopStories(searchTerm);
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    
     event.preventDefault();
   }
 
   onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
     const isNodId = item => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNodId);
+    const updatedHits = hits.filter(isNodId);
 
     this.setState({
-      result: {...this.state.result, hits: updatedHits},
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      },
     });
   }
 
@@ -89,7 +103,8 @@ class App extends Component {
     const {
       searchTerm,
       results,
-      searchKey
+      searchKey,
+      error
     } = this.state;
 
     const page = (
@@ -115,19 +130,23 @@ class App extends Component {
             Search
           </Search>
         </div>
-          { list && 
+        { error
+          ? <div className="interactions">
+              <p>Something went wrongt.</p>
+            </div>
+          :
             <Table
               list={list}
               onDismiss={this.onDismiss}
             />
-          }
-          <div className="interactions">
-            <Button
-              onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
-            >
-              More
-            </Button>
-          </div>
+        }
+        <div className="interactions">
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+          >
+            More
+          </Button>
+        </div>
       </div>
     );
   }
